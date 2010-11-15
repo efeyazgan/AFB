32e22d0db0c0fbf9decca3de85b3afcea8eef1d5
efe <>
2010-11-15 13:36:08 +0000

    *** empty log message ***

--- test/tree_cfg.py
+++ test/tree_cfg.py
@@ -70,7 +70,7 @@ process.oneGoodVertexFilter = cms.EDFilter("VertexSelector",
 
 process.mu_HLT = cms.EDFilter("HLTHighLevel",
      TriggerResultsTag = cms.InputTag("TriggerResults","","HLT"),
-     HLTPaths = cms.vstring('HLT_Mu9','HLT_Mu11','HLT_Mu15_v1'),   # provide list of HLT paths (or patterns) you want
+     HLTPaths = cms.vstring('HLT_Mu9','HLT_Mu15_v1'),   # provide list of HLT paths (or patterns) you want
      eventSetupPathsKey = cms.string(''), # not empty => use read paths from AlCaRecoTriggerBitsRcd via this key
      andOr = cms.bool(True),              # how to deal with multiple triggers: True (OR) accept if ANY is true, False (AND) accept if ALL are true
      throw = cms.bool(True)    # throw exception on unknown path names
